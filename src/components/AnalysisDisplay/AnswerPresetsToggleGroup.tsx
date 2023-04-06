5eadf7b6fb7273ff0892a96ac5facf730bdb6dc8
Jason Jun <jason.jun@envato.com>
2023-04-06 21:26:35 +1000

    feat: autofocus on custom field

--- src/components/AnalysisDisplay/AnswerPresetsToggleGroup.tsx
+++ src/components/AnalysisDisplay/AnswerPresetsToggleGroup.tsx
@@ -1,21 +1,6 @@
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
-import { CheckIcon } from '@heroicons/react/20/solid'
 import useAutoHeightTextArea from '@/hooks/useAutoHeightTextArea'
-
-const ToggleGroupItem = ({ value, children, tabIndex }) => {
-  return (
-    <ToggleGroup.Item
-      value={value}
-      tabIndex={tabIndex}
-      className="group rounded-xl bg-gray-200 p-0.5 text-gray-500 transition-colors hover:bg-gray-300 focus:bg-suit focus:outline-none data-[state=on]:bg-suit data-[state=on]:text-blue-850"
-    >
-      <div className="flex items-start justify-between gap-6 rounded-[10px] bg-white p-4 text-left outline outline-white transition-all duration-75 ease-linear hover:outline-gray-300 group-data-[state=on]:bg-blue-25 group-data-[state=on]:outline-none">
-        <span className="flex w-full">{children}</span>
-        <CheckIcon className="h-[22px] w-[22px] shrink-0 rounded-full border border-gray-200 bg-white p-1 text-white group-data-[state=on]:border-blue-850 group-data-[state=on]:bg-blue-850" />
-      </div>
-    </ToggleGroup.Item>
-  )
-}
+import ToggleGroupItem from './ToggleGroupItem'
 
 const AnswerPresetsToggleGroup = ({ list, value, setValue }) => {
   const textAreaRef = useAutoHeightTextArea()
@@ -38,7 +23,13 @@ const AnswerPresetsToggleGroup = ({ list, value, setValue }) => {
           {item}
         </ToggleGroupItem>
       ))}
-      <ToggleGroupItem value={numberOfItems + 1} tabIndex={numberOfItems}>
+      <ToggleGroupItem
+        value={numberOfItems + 1}
+        tabIndex={numberOfItems}
+        onClick={() => {
+          textAreaRef.current?.focus()
+        }}
+      >
         <textarea
           ref={textAreaRef}
           placeholder="직접 작성하기"
---
+++ src/components/AnalysisDisplay/ToggleGroupItem.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+import { CheckIcon } from '@heroicons/react/20/solid'
+
+interface ToggleGroupItemProps {
+  value: string
+  children: ReactNode
+  tabIndex: number
+  onClick?: () => void
+}
+
+const ToggleGroupItem = ({
+  value,
+  children,
+  tabIndex,
+  onClick,
+}: ToggleGroupItemProps) => {
+  return (
+    <ToggleGroup.Item
+      value={value}
+      tabIndex={tabIndex}
+      className="group rounded-xl bg-gray-200 p-0.5 text-gray-500 transition-colors hover:bg-gray-300 focus:bg-suit focus:outline-none data-[state=on]:bg-suit data-[state=on]:text-blue-850"
+      onClick={onClick}
+    >
+      <div className="flex items-start justify-between gap-6 rounded-[10px] bg-white p-4 text-left outline outline-white transition-all duration-75 ease-linear hover:outline-gray-300 group-data-[state=on]:bg-blue-25 group-data-[state=on]:outline-none">
+        <span className="flex w-full">{children}</span>
+        <CheckIcon className="h-[22px] w-[22px] shrink-0 rounded-full border border-gray-200 bg-white p-1 text-white group-data-[state=on]:border-blue-850 group-data-[state=on]:bg-blue-850" />
+      </div>
+    </ToggleGroup.Item>
+  )
+}
+
+ToggleGroupItem.defaultProps = {
+  onClick: () => {},
+}
+
+export default ToggleGroupItem
