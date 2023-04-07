fc243a4d103504624231bb5158b7a85f541d6b87
Jason Jun <jason.jun@envato.com>
2023-04-07 21:02:07 +1000

    style: toggle item border

--- src/components/ToggleGroupItem.tsx
+++ src/components/ToggleGroupItem.tsx
@@ -19,7 +19,7 @@ const ToggleGroupItem = ({
     <ToggleGroup.Item
       value={value}
       tabIndex={tabIndex}
-      className="group rounded-xl bg-gray-200 p-0.5 text-gray-500 transition-colors hover:bg-gray-300 focus:bg-suit focus:outline-none data-[state=on]:bg-suit data-[state=on]:text-blue-850"
+      className="group rounded-xl bg-gray-300 p-0.5 text-gray-500 transition-colors hover:bg-gray-300 focus:bg-suit focus:outline-none data-[state=on]:bg-suit data-[state=on]:text-blue-850"
       onClick={onClick}
     >
       <div className="flex items-start justify-between gap-6 rounded-[10px] bg-white p-4 text-left outline outline-white transition-all duration-75 ease-linear hover:outline-gray-300 group-data-[state=on]:bg-blue-25 group-data-[state=on]:outline-none">
