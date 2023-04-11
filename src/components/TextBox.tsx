ea79b150bdcc7acfbee57aaad08bbbaf9cf53d1c
Jason Jun <jason.jun@envato.com>
2023-04-11 15:04:41 +1000

    feat: set maximum character limit

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import Button from '@/components/Button'
 import ErrorMessage from '@/components/ErrorMessage'
 import useAutoHeightTextArea from '@/hooks/useAutoHeightTextArea'
@@ -7,6 +7,7 @@ interface TextBoxProps {
   placeholder?: string
   value: string
   errorMessage?: string
+  maxLength?: number
   [x: string]: any
   button: {
     label: string
@@ -20,21 +21,34 @@ const TextBox = ({
   onChange,
   placeholder,
   errorMessage,
+  maxLength,
   button,
 }: TextBoxProps) => {
+  const [textCount, setTextCount] = useState(0)
   const textAreaRef = useAutoHeightTextArea(2)
+
+  const handleTextAreaChange = e => {
+    onChange(e)
+    setTextCount(e.target.value.length)
+  }
 
   return (
     <div className="flex min-h-[400px] w-full max-w-[800px] flex-col rounded-xl  bg-white drop-shadow-xs">
       <textarea
         ref={textAreaRef}
         value={value}
-        onChange={onChange}
+        onChange={handleTextAreaChange}
         placeholder={placeholder}
+        maxLength={maxLength}
         className="h-full w-full grow resize-none rounded-t-xl border border-gray-200 p-6 outline-none transition-all duration-75 hover:border-grayBlue-200 focus:border-grayBlue-300"
       />
       <div className="flex items-center justify-end gap-6 rounded-b-xl border border-t-0 border-gray-200 p-6">
         {errorMessage && <ErrorMessage text={errorMessage} />}
+        {textCount > maxLength * 0.9 && (
+          <div className="text-sm text-gray-400">
+            {textCount} / {maxLength}
+          </div>
+        )}
         <Button
           label={button.label}
           onClick={button.onClick}
@@ -48,6 +62,7 @@ const TextBox = ({
 TextBox.defaultProps = {
   placeholder: '',
   errorMessage: null,
+  maxLength: 10000,
 }
 
 export default TextBox
