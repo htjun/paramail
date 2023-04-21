334af2f757e55786fd30a45161b23ce5aea5ef74
Jason Jun <jason.jun@envato.com>
2023-04-21 21:37:27 +1000

    add facebook login

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { useSession, signIn } from 'next-auth/react'
 import { twMerge } from 'tailwind-merge'
 import Navigation from '@/components/Navigation'
+import { UserCircleIcon } from '@heroicons/react/24/outline'
 import { sectionContainer, buttonSecondary } from '@/styles/sharedClasses'
 
 const LoginItem = ({ method, label }) => {
@@ -41,9 +42,13 @@ const LoginPage = () => {
             'flex w-full max-w-lg flex-col gap-6 px-6 py-8'
           )}
         >
-          <h2 className="text-gray-600">원하시는 로그인 방식을 선택하세요</h2>
-          <div>
+          <h2 className="flex items-center gap-2 text-gray-600">
+            <UserCircleIcon className="h-6 w-6 text-gray-400" />
+            <span>원하시는 로그인 방식을 선택하세요</span>
+          </h2>
+          <div className="flex flex-col gap-3">
             <LoginItem method="google" label="구글" />
+            <LoginItem method="facebook" label="페이스북" />
           </div>
         </div>
       </main>
