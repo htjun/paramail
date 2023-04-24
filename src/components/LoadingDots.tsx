d40605af0a3f6dcd0c58ea9029dd97ec7b9ef01f
Jason Jun <jason.jun@envato.com>
2023-04-24 20:54:29 +1000

    Update loading dots color

--- src/components/LoadingDots.tsx
+++ src/components/LoadingDots.tsx
@@ -3,9 +3,9 @@ import styles from '@/styles/loading-dots.module.css'
 const LoadingDots = () => {
   return (
     <span className={styles.loading}>
-      <span className="bg-blue-800" />
-      <span className="bg-blue-800" />
-      <span className="bg-blue-800" />
+      <span className="bg-indigo-800" />
+      <span className="bg-indigo-800" />
+      <span className="bg-indigo-800" />
     </span>
   )
 }
