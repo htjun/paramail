53d10d495a2edfa63701c2c3c481343a6a6103c2
Jason Jun <jason.jun@envato.com>
2023-04-26 18:54:05 +1000

    prompt optimization

--- src/pages/api/generate/promptData.ts
+++ src/pages/api/generate/promptData.ts
@@ -6,50 +6,44 @@ export const analysisPromptMessages = (
   return [
     {
       role: 'system',
-      content: `You are a smart assistant working for the recipient of this email who is Korean. Summarize this email and list up to 5 action points if needed. Prefix each with this exact symbol _^. Also come up with up to 3 possible answers to this email in Korean, straight to the point with no greeting, and in less than 20 words. Prefix each with _^.`,
+      content: `Summarize, list up to 5 actions with _^, provide up to 3 answers in <20 words, each with _^. All in Korean.`,
     },
     {
       role: 'user',
-      content: `Hey Tom, Hope you're doing well! I saw those pictures from your weekend hike on Instagram – looked like a ton of fun! We should plan a group hike sometime. On a different note, we're organizing an office potluck next Friday, April 14th. We thought it'd be a great opportunity to unwind and enjoy each other's company before the quarterly review. If you're in, just let me know what dish you'd like to bring, and I'll add it to the list! Also, I wanted to quickly check in on the progress of the website redesign project. Do you think you'll have the initial mockups ready for review by next Wednesday, April 12th? No worries if you need more time, just give me a heads up so I can adjust the schedule accordingly. Looking forward to the potluck and your update! Take care, Mia`,
+      content: `Tom, Saw your hike pics, let's plan one too. Office potluck April 14th, let me know your dish. Need website mockups by April 12th or update if more time needed.
+      Mia`,
     },
     {
       role: 'assistant',
-      content: `Summary: 미아가 인스타그램에 올린 주말 하이킹 사진을 보았다고 하며, 그룹 하이킹을 계획해보자고 합니다. 또한, 4월 14일 금요일에 사무실 팟럭을 계획 중이며, 참석 여부와 가져올 음식을 알려 달라고 요청합니다. 마지막으로 웹사이트 리디자인 프로젝트 진행 상황에 대해 문의하였으며, 4월 12일 수요일까지 초기 목업을 검토할 수 있는지 물었습니다.
+      content: `Summary: 미아가 톰에게 그룹 하이킹을 계획하자고 제안하며, 4월 14일 사무실 팟럭 참여 의사와 가져올 음식을 물어보고, 웹사이트 리디자인 프로젝트의 초기 목업을 4월 12일까지 제출해달라는 요청을 보내왔습니다.
 
       Action points:
       _^ 그룹 하이킹 계획에 대해 답장하세요.
-      _^ 팟럭 참여 의사와 가져올 음식을 미아에게 알려주세요.
-      _^ 웹사이트 리디자인 프로젝트의 초기 목업 검토 가능 여부를 미아에게 알려주세요.
-      
+      _^ 4월 14일 팟럭 참여 의사와 가져올 음식을 미아에게 알려주세요.
+      _^ 웹사이트 리디자인 프로젝트의 초기 목업을 4월 12일까지 제출하세요.
+            
       Possible answers:
-      _^ 물론이죠, 저는 행운을 빌어요. 샐러드를 가져올게요. 다음 주 수요일까지는 웹사이트 모형이 준비될 것입니다.
-      _^ 다음 달에 단체 하이킹을 계획해요. 저는 포트럭을 위해 디저트를 가져올 것입니다. 모의고사는 다음 주 수요일까지 준비될 것입니다.
-      _^ 초대해 주셔서 감사합니다만, 저는 포트럭에 갈 수 없을 것 같습니다. 모의고사는 다음 주 수요일까지 준비될 것입니다.`,
+      _^ 그룹 하이킹 재미있겠네요. 팟럭에는 샌드위치를 가져갈게요.
+      _^ 그룹 하이킹은 어려울것 같아요. 팟럭에는 도넛을 가져갈게요.
+      _^ 그룹 하이킹 좋습니다. 4월 14일은 일정이 있어서 팟럭에 참여하기 어려울것 같네요.`,
     },
     {
       role: 'user',
-      content: `Subject: Launch Stuff & Coffee?
-
-      Hey Samantha, How's it going? I heard you went to Hawaii recently - can't wait to hear all about it!
-      So, we've got that product launch coming up, and I was hoping you could help with a couple of things. Can you take a look at the marketing stuff (attached) and let me know what you think? Also, we need some social media ideas - any suggestions? Oh, and we should talk about the launch event. Maybe reach out to Emily Thompson? Let's catch up over coffee next week to discuss everything and share Hawaii stories!
-      
-      Thanks a bunch!
-      
+      content: `Samantha, Need your input on marketing (attached) and social media ideas. Contact Emily for launch event? Let's discuss over coffee next week.
       Alex`,
     },
     {
       role: 'assistant',
-      content: `Summary: 알렉스가 새 제품 출시를 준비 중이며, 마케팅 자료와 소셜 미디어 아이디어를 공유해 달라는 요청과 함께 런칭 이벤트에 대해 이야기하고 싶다는 내용입니다.
+      content: `Summary: 알렉스가 마케팅 자료와 소셜 미디어 아이디어를 공유하고, 런칭 이벤트에 대해 이메일을 보내는 것을 제안하며, 다음 주에 커피챗을 하자고 합니다.
 
       Action points:
-      _^ 마케팅 자료를 검토하고 의견을 알려주세요.
-      _^ 소셜 미디어 아이디어를 제안해주세요.
-      _^ 이벤트에 대해 이야기하고 싶으니 다음 주 커피를 마시면서 만나서 이야기해봅시다.
-      
+      _^ 첨부 된 마케팅 자료와 소셜 미디어 아이디어를 검토하고 제안해주세요.
+      _^ 에밀리에게 연락해서 출시 행사를 논의하세요.
+      _^ 커피챗 일정을 제안하세요.
+            
       Possible answers:
-      _^ 마케팅 자료와 소셜 미디어 아이디어를 검토하고 제안해드리겠습니다. 다음 주에 커피를 마시면서 만나서 이야기해봅시다.
-      _^ 하와이 여행은 좋았습니다. 마케팅 자료와 소셜 미디어 아이디어를 검토하고 제안해드리겠습니다. 다음 주에 커피를 마시면서 만나서 이야기해봅시다.
-      _^ 마케팅 자료와 소셜 미디어 아이디어를 검토하고 제안해드리겠습니다. 다음 주에는 시간이 없어서 만날 수 없습니다.`,
+      _^ 마케팅 자료와 소셜 미디어 아이디어는 확인했습니다. 에밀리와는 곧 연락해볼게요. 다음 주 화요일 커피챗 어떠세요?
+      _^ 마케팅 자료와 소셜 미디어 아이디어 곧 확인하겠습니다. 에밀리와도 연락해볼게요. 커피챗은 다음 주 언제가 가능하신가요?`,
     },
     {
       role: 'user',
