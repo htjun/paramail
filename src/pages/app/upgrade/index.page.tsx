bf03b99b45c98bc4acc7ed599a68e104ad0fb7d6
Jason Jun <jason.jun@envato.com>
2023-05-15 14:53:59 +1000

    scope route protecting for plan card

--- src/pages/app/upgrade/index.page.tsx
+++ src/pages/app/upgrade/index.page.tsx
@@ -25,40 +25,39 @@ const UpgradePage = ({ plans }: { plans: PlanProps[] }) => {
     }
   }, [profile])
 
-  if (!isLoading && !!profile)
-    return (
-      <>
-        <Meta title="업그레이드" />
-        <AppNavigation />
-        <main className={notoSansKR.className}>
-          <div className="align-center mb-16 mt-20 flex flex-col items-center gap-10">
-            <div className="inline-flex h-10 rounded-full bg-magic p-[1px]">
-              <div className="inline-flex h-full shrink-0 items-center gap-2 rounded-full bg-white px-4 text-[#F07E78]">
-                <WandSVG className="h-4 w-4" />
-                <span className="bg-magic bg-clip-text text-sm font-medium [-webkit-text-fill-color:transparent]">
-                  업그레이드
-                </span>
-              </div>
+  return (
+    <>
+      <Meta title="업그레이드" />
+      <AppNavigation />
+      <main className={notoSansKR.className}>
+        <div className="align-center mb-16 mt-20 flex flex-col items-center gap-10">
+          <div className="inline-flex h-10 rounded-full bg-magic p-[1px]">
+            <div className="inline-flex h-full shrink-0 items-center gap-2 rounded-full bg-white px-4 text-[#F07E78]">
+              <WandSVG className="h-4 w-4" />
+              <span className="bg-magic bg-clip-text text-sm font-medium [-webkit-text-fill-color:transparent]">
+                업그레이드
+              </span>
             </div>
-            <h1 className={twMerge(marketingPageTitle, 'max-w-lg')}>
-              <div>더 많은 기능을</div>
-              <div>더 여유롭게 사용하세요</div>
-            </h1>
-            <p className={marketingPageSubtitle}>
-              언제든지 구독을 중단하실 수 있습니다.
-            </p>
           </div>
+          <h1 className={twMerge(marketingPageTitle, 'max-w-lg')}>
+            <div>더 많은 기능을</div>
+            <div>더 여유롭게 사용하세요</div>
+          </h1>
+          <p className={marketingPageSubtitle}>
+            언제든지 구독을 중단하실 수 있습니다.
+          </p>
+        </div>
+        {!isLoading && !!profile && (
           <div className="mb-32 flex flex-col items-center justify-center gap-8 px-6 md:flex-row md:items-stretch">
             <FreePlanCard user={{ plan: 'business' }} />
             {plans.map(plan => (
               <PlanCard key={plan.id} {...plan} user={{ plan: 'business' }} />
             ))}
           </div>
-        </main>
-      </>
-    )
-
-  return null
+        )}
+      </main>
+    </>
+  )
 }
 
 export const getStaticProps = async () => {
